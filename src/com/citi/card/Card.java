250e982256fd6e66723e663ce498cc41d2bbd8ee
mplusf <minal.fulzele@yahoo.com>
2017-06-04 06:41:43 +0000

    first commit

---
+++ src/com/citi/card/Card.java
@@ -0,0 +1,11 @@
+package com.citi.card;
+
+/**
+ * == electronic pre-paid cash card ==
+ *
+ * Loading money onto the card.
+ · Spending credit balances from the card.
+ · Using the card simultaneously at multiple retailers.
+ */
+public class Card {
+}
---
+++ test/com/citi/card/CardTest.java
@@ -0,0 +1,12 @@
+package com.citi.card;
+
+/**
+ * == electronic pre-paid cash card ==
+ *
+ * Loading money onto the card.
+ · Spending credit balances from the card.
+ · Using the card simultaneously at multiple retailers.
+ */
+public class CardTest {
+
+}
